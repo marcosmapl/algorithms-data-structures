d4e7ab57a490dd4b7b10be11aa88ccedc7f23be8
Marcos Lima <37479099+limadmarcos@users.noreply.github.com>
2018-08-25 11:20:49 -0400

    Update nim_game.py
    Bug fixes
    1 - Inverted game socore
    2 - Player making automatic decision

--- nim_game.py
+++ nim_game.py
@@ -8,25 +8,28 @@ __status__ = "Published"
 
 def computador_escolhe_jogada(n,m):
   jogada = m
-  elif (n % (m+1)):
+  if (n % (m+1)):
     jogada = (n % (m+1))
-  print('\nComputer has removed {pieces}.'.format(pieces='one piece' if jogada == 1 else str(jogada)+' pieces'))
+  print('\nComputer decided to remove {pieces}.'.format(pieces='one piece' if jogada == 1 else str(jogada)+' pieces'))
   return jogada
 
 def usuario_escolhe_jogada(n,m):
-  jogada = m+1
-  while (jogada > m) and (jogada < 1):
-    jogada = int(input('\nHow many pieces did you want to remove? '))
-    if (jogada > m):
+  jogada = 0
+  while (jogada > m) or (jogada < 1):
+    jogada = int(input('\nHow many pieces would you like to remove? '))
+    if (jogada > m) or (jogada < 1):
       print('\nOops! Invalid quantity! Try again.')
-  print('\nYou has removed {pieces}.'.format(pieces='one piece' if jogada == 1 else str(jogada)+' pieces'))
+  print('\nYou removed {pieces}.'.format(pieces='one piece' if jogada == 1 else str(jogada)+' pieces'))
   return jogada
 
 def partida():
-  n = int(input('How many pieces in this game? '))
+  n = int(input('\nHow many pieces in this game? '))
   m = int(input('Limit of removed pieces per move? '))
-  vez = 0
   
+  # vez 1 - player turn
+  # vez 0 - computer turn
+  vez = 0
+
   if (m > n) or (n % (m+1)):
     print('\nComputer starts the game!')
   else:
@@ -39,14 +42,17 @@ def partida():
     else:
       jogada = computador_escolhe_jogada(n,m)
     n = n - jogada  
+    vez = not vez
     if (n > 0):
       print('Now {pieces} in game.'.format(pieces='there is only one piece' if n == 1 else 'there are '+str(n)+' pieces'))
-      vez = not vez      
-      
+  
+  # if the game ends with player turn, computer did last move and wins.
+  # otherwise, player did last move and wins
   if (vez):
-    print('The Game is Over! You wins!\n')
+    print('The Match is Over! Computer wins!\n')
   else:
-    print('The Game is Over! Computer wins!\n')
+    print('The Match is Over! You wins!\n')
+    
   return vez
 
 
@@ -55,11 +61,13 @@ def campeonato():
   pontosCPU = 0
   pontosJogador = 0
   for i in range(1,4):
-    print('**** Round {} ****\n'.format(i))
+    print('**** Round {} ****'.format(i))
+    # partida returns zero if player wins
+    # and one for computer wins
     if(partida()):
-      pontosJogador += 1
-    else:
       pontosCPU +=1
+    else:
+      pontosJogador += 1
   print('**** End of championship! ****\n')
   print('Game Score: You {} x {} Computer'.format(pontosJogador,pontosCPU))
   return 0
