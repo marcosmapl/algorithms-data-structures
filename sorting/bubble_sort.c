a7290529a8dab8be4b4c50e012abb429abd56af7
Marcos Lima <marcos.lima@icomp.ufam.edu.br>
2020-03-15 21:59:25 -0400

    Add an improvemento to Bubble Sort
    Now bubble sort has a flag and stops when no swap occurs

--- sorting/bubble_sort.c
+++ sorting/bubble_sort.c
@@ -13,8 +13,19 @@ void printArray(int *v, unsigned int size) {
 
 void bubbleSort(int *v, unsigned int size) {
     unsigned int i,j, k = size;
+    unsigned char swap;
     
+    // k splits the array in two partitions
+    // right side is ordered (i >= k)
+    // left-side is unordered (i < k)
     while (--k) {
+        
+        // swap is a improvement
+        // it stops the bubble sort if array is ordered
+        swap = 0;
+        
+        // now we pass through the array
+        // swapping the greater elementos to the right-side
         for (i = 0, j = 1; i < k; i++, j++) 
         {
             if (v[i] > v[j]) 
@@ -22,9 +33,15 @@ void bubbleSort(int *v, unsigned int size) {
                 unsigned int temp = v[i];
                 v[i] = v[j];
                 v[j] = temp;
+                swap = 1;
             }
         }
         printArray(v, size);
+        
+        // if we didn't swap any element
+        // then the array is ordered, we can stop here
+        if (!swap)
+            break;
     }
 }
 
